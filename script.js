7707e1c3c31529f5739c3b40533e0ad156ad5a36
Tri Pham <trcpham@ucsc.edu>
2023-04-26 18:39:02 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+let latitude = 0
+let longitude = 0
+
+window.onload = function() {
+    const date = new Date()
+    const dateString = (date.getMonth()+1) + '/' + date.getDate() +
+     '/' +date.getFullYear();
+     document.getElementById('date').textContent = dateString
+}    
+window.document.getElementById(button)
