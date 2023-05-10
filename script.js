e1c4fb8d685b2d3af9b4547ed07f761232d5586d
Tri Pham <trcpham@ucsc.edu>
2023-05-09 18:26:11 -0700

    Update script.js

--- script.js
+++ script.js
@@ -22,9 +22,42 @@ function success(position){
 const btn = document.getElementById('getWeatherBtn');
 
 btn.addEventListener("click",function() {
-    let forecast = [["M", 52], ["Tu", 53], ["W", 54], ["Th", 55], ["F", 56]]
+ const xhr = new XMLHttpRequest();
+xhr.open("GET", `http://localhost:3000/weather/${latitude}/${longitude}`);
+xhr.send ();
+xhr.onload = function() {
+     const body = JSON.parse(xhr.responseText) 
+     let temperature = body.temperature;
+     let weatherStatus = body.weatherStatus;
+     document.getElementById('temperature').innerHTML =`Temperature: ${temperature} F`;
+     document.getElementById('weatherStatus').innerHTML= `Weather Status: ${weatherStatus}`;
+
+ }
+ 
+//-----------------------5day forecast (xhr2)----------------------------- 
+
+const xhr2 = new XMLHttpRequest();
+xhr2.open("GET", `http://localhost:3000/weather/${lat}/${lon}`);
+xhr2.send();
+
+xhr2.onload = function() {
+	const body = JSON.parse(xhr.responseText)
+    let forecast = body.forecast;
+    let forecastElements = document.getElementsByClassName("forecast");
+    for (let i= 0; i < forecast.length; i++){
+        forecastElements[i].innerHTML = `${forecast[i].dayName}: ${forecast[i].temp}°F`;
+    }
+    
+
+  
+  
+    
+    
+      }
+});
+let forecast = [["M", 52], ["Tu", 53], ["W", 54], ["Th", 55], ["F", 56]]
     let forecastElements = document.getElementsByClassName("forecast");
     for (let i = 0; i < forecast.length; i++) {
         forecastElements[i].innerHTML = forecast[i][0] + ": " + forecast[i][1] + "°F";
-      }
-});
+
+    }
