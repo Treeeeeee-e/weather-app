460211cd41e293d9842fa830964b2b5f3dd4561a
Tri Pham <trcpham@ucsc.edu>
2023-05-09 20:46:31 -0700

    Update server.js

--- wapi/server.js
+++ wapi/server.js
@@ -4,20 +4,22 @@ const request = require("request");
 const app = express();
 
 
+const API_KEY = `e3f24db3752c7c6d86c1af5dcc64797c`
 
 
 
-
-app.get('/', (req, res) => {
+app.get('/weather/:lat/:lon', (req, res) => {
   res.send('Hello World!');
   console.log("welcome to the root!");
+  let lon = req.params['lon']
+  let lat = req.params ['lat']
 
-const API_KEY = `e3f24db3752c7c6d86c1af5dcc64797c`
-let lon = 122.41942
-let lat = 37.77493
+
+
   
   var url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=imperial`
-
+  
+let body=JSON.parse(body)
   
   
 	request(url, (error, response, body)=>{
